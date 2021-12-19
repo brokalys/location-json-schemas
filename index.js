3bdaecd407f8a9bba400a1b32b237187a429ff1c
Matiss Janis Aboltins <matiss@mja.lv>
2021-12-19 20:52:15 +0000

    :bookmark:  1.3.0: relationships between locations (easy parent-child loc)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const riga = require('./data/riga.json');
 const latvia = require('./data/latvia.json');
+const latviaRelationships = require('./data/latvia-relationships.json');
 
 module.exports = {
   riga,
   latvia,
+  latviaRelationships,
 };
