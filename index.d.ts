63f8542f395f36edd6aea79169f505961aaaada4
Matiss Janis Aboltins <matiss@mja.lv>
2022-05-08 20:31:52 +0100

    :bookmark:  1.4.0: add type definitions

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+interface BaseProperties {
+  name: string;
+  id: string;
+}
+
+export type LatviaFeatureProperties = { Level: number } & BaseProperties;
+export type LatviaGeoJsonFeature = GeoJSON.Feature<
+  GeoJSON.MultiPolygon,
+  LatviaFeatureProperties
+>;
+
+export type RigaFeatureProperties = { gid: number } & BaseProperties;
+export type RigaGeoJsonFeature = GeoJSON.Feature<
+  GeoJSON.Polygon,
+  RigaFeatureProperties
+>;
+
+export const latvia: GeoJSON.FeatureCollection<
+  GeoJSON.MultiPolygon,
+  LatviaFeatureProperties
+>;
+export const riga: GeoJSON.FeatureCollection<
+  GeoJSON.Polygon,
+  RigaFeatureProperties
+>;
+export const latviaRelationships: Record<string, string[]>;
